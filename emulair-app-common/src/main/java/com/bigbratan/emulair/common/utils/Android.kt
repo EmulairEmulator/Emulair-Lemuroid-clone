c8c6f835f1a4d2e11beaefc9eace0f6e54a09b54
Radu Bratan <hyperbeast.mlgboss@gmail.com>
2023-02-28 15:34:26 +0200

    added Android file

---
+++ emulair-app-common/src/main/java/com/bigbratan/emulair/common/utils/Android.kt
@@ -0,0 +1,38 @@
+package com.bigbratan.emulair.utils
+
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import android.widget.Toast
+
+fun Bundle?.dump(): String {
+    if (this == null) return "null"
+
+    val builder = StringBuilder("Extras:\n")
+    keySet()
+        .toSet()
+        .forEach { key ->
+            builder.append(key).append(": ").append(get(key)).append("\n")
+        }
+    return builder.toString()
+}
+
+fun Activity.displayToast(string: String, length: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, string, length).show()
+}
+
+fun Activity.displayToast(stringId: Int, length: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, stringId, length).show()
+}
+
+fun Context.animationDuration(): Int {
+    return resources.getInteger(android.R.integer.config_mediumAnimTime)
+}
+
+fun Context.shortAnimationDuration(): Int {
+    return resources.getInteger(android.R.integer.config_shortAnimTime)
+}
+
+fun Context.longAnimationDuration(): Int {
+    return resources.getInteger(android.R.integer.config_longAnimTime)
+}
