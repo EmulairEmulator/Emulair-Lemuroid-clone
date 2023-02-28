b2ea031cb3765a9e0b3b4a41eeb7ad50dcd123b4
Radu Bratan <hyperbeast.mlgboss@gmail.com>
2023-02-28 14:05:29 +0200

    added gradle and git files

---
+++ build.gradle.kts
@@ -0,0 +1,132 @@
+import com.android.build.gradle.BaseExtension
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath(deps.plugins.android)
+        classpath(deps.plugins.navigationSafeArgs)
+    }
+}
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version deps.versions.kotlin
+    id("com.github.ben-manes.versions") version "0.39.0"
+    id("org.jmailen.kotlinter") version "3.0.2"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.4.0"
+    id("name.remal.check-dependency-updates") version "1.5.0"
+    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    checkstyle
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        mavenLocal()
+        mavenCentral()
+        maven { setUrl("https://jitpack.io") }
+    }
+
+    apply(plugin = "org.jmailen.kotlinter")
+
+    kotlinter {
+        // We are currently disabling tests for import ordering.
+        disabledRules = arrayOf("import-ordering")
+    }
+
+    configurations.all {
+        resolutionStrategy.eachDependency {
+            when (requested.group) {
+                "com.google.android.gms" -> useVersion(deps.versions.gms)
+                "org.jetbrains.kotlin" -> {
+                    if (requested.name.startsWith("kotlin-stdlib-jre")) {
+                        with(requested) {
+                            useTarget("$group:${name.replace("jre", "jdk")}:$version")
+                        }
+                    }
+                    useVersion(deps.versions.kotlin)
+                }
+            }
+        }
+    }
+}
+
+subprojects {
+    apply {
+        plugin("checkstyle")
+    }
+
+    afterEvaluate {
+        tasks {
+            val checkstyle by creating(Checkstyle::class) {
+                configFile = file("$rootDir/config/checkstyle/checkstyle.xml")
+                classpath = files()
+                source("src")
+            }
+            findByName("check")?.dependsOn(checkstyle)
+        }
+
+        extensions.configure(CheckstyleExtension::class.java) {
+            isIgnoreFailures = false
+            toolVersion = "8.8"
+        }
+
+        if (hasProperty("android")) {
+            // BaseExtension is common parent for application, library and test modules
+            extensions.configure(BaseExtension::class.java) {
+                compileSdkVersion(deps.android.compileSdkVersion)
+                buildToolsVersion(deps.android.buildToolsVersion)
+                defaultConfig {
+                    minSdkVersion(deps.android.minSdkVersion)
+                    targetSdkVersion(deps.android.targetSdkVersion)
+                    multiDexEnabled = true
+                }
+                lintOptions {
+                    isAbortOnError = true
+                    disable("UnusedResources") // https://issuetracker.google.com/issues/63150366
+                    disable("InvalidPackage")
+                    disable("VectorPath")
+                    disable("TrustAllX509TrustManager")
+                }
+                dexOptions {
+                    dexInProcess = true
+                }
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_1_8
+                    targetCompatibility = JavaVersion.VERSION_1_8
+                }
+            }
+        }
+    }
+
+    configurations {
+        all {
+            exclude(group = "com.google.code.findbugs", module = "jsr305")
+        }
+    }
+}
+
+tasks {
+    "clean"(Delete::class) {
+        delete(buildDir)
+    }
+
+    "dependencyUpdates"(DependencyUpdatesTask::class) {
+        resolutionStrategy {
+            componentSelection {
+                all {
+                    val rejected = listOf("alpha", "beta", "rc", "cr", "m")
+                        .map { qualifier -> Regex("(?i).*[.-]$qualifier[.\\d-]*") }
+                        .any { it.matches(candidate.version) }
+                    if (rejected) {
+                        reject("Release candidate")
+                    }
+                }
+            }
+        }
+    }
+}
