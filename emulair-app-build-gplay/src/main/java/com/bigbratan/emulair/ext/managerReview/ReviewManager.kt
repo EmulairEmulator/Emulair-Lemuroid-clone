b633b7eda9393dc8a63613e1748dd2c767ccb629
Bratan Radu <hyperbeast.mlgboss@gmail.com>
2023-03-31 16:10:45 +0300

    Modified some deps

--- emulair-app-build-gplay/src/main/java/com/bigbratan/emulair/ext/managerReview/ReviewManager.kt
+++ emulair-app-build-gplay/src/main/java/com/bigbratan/emulair/ext/managerReview/ReviewManager.kt
@@ -21,7 +21,7 @@ class ReviewManager {
     }
 
     suspend fun launchReviewFlow(activity: Activity, sessionTimeMillis: Long) {
-        // Only sessions which lasted more than 10 minutes considered good sessions
+        // Only sessions which lasted more than 10 minutes are considered good sessions
         if (sessionTimeMillis < MIN_GAME_SESSION_LENGTH) {
             return
         }
