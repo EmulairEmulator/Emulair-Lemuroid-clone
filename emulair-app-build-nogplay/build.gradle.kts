d12f6e53e7ca3dcaa5a475ed56f509d67a60948a
RaduBratan <bratanradu7@gmail.com>
2023-11-16 20:54:19 +0200

    add deps for storage access

--- emulair-app-build-nogplay/build.gradle.kts
+++ emulair-app-build-nogplay/build.gradle.kts
@@ -15,4 +15,9 @@ dependencies {
     implementation(project(":emulair-app-common"))
     implementation(deps.libs.retrofit)
     implementation(deps.libs.kotlinxCoroutinesAndroid)
+
+    implementation(deps.libs.androidx.appcompat.appcompat)
+    implementation(deps.libs.androidx.appcompat.constraintLayout)
+    implementation(deps.libs.material)
+    implementation("androidx.documentfile:documentfile:1.0.1")
 }
